bf937544923384cccfdc37f96742c29c2c2deb34
Stanislas Trillard <stanislas.trillard@etu.unilim.fr>
2024-09-22 19:09:07 +0200

    Add project files.

---
+++ GameData.cs
@@ -0,0 +1,40 @@
+﻿namespace SubnauticaBelowZeroArtemisGSI
+{
+    public class GameData
+    {
+        public GameData() {
+            oxygen_available = 0;
+            oxygen_available = 0;
+            health = 0;
+            max_health = 100;
+            food = 0;
+            water = 0;
+            body_temperature = 0;
+            depth_level = 0;
+            biome = "";
+            rain_intensity = 0;
+            snow_intensity = 0;
+            is_swimming = false;
+            is_inside = false;
+            PDA_state = PDA.State.Closed;
+        }
+        public float oxygen_available { get; set; }
+        public float oxygen_capacity { get; set; }
+        public float health { get; set; }
+        public float max_health { get; set; }
+        public float food { get; set; }
+        public float water { get; set; }
+        public float body_temperature { get; set; }
+        public float depth_level { get; set; }
+        public string biome { get; set; }
+        public float rain_intensity { get; set; }
+        public float snow_intensity { get; set; }
+        public bool is_swimming { get; set; }
+        public bool is_inside { get; set; }
+        public PDA.State PDA_state { get; set; }
+    }
+
+    public enum Biomes{
+        // TODO
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,91 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System.Net.Http;
+using System;
+using System.Reflection;
+using Newtonsoft.Json;
+using System.Text;
+using System.IO;
+using System.Linq;
+
+namespace SubnauticaBelowZeroArtemisGSI
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    [BepInDependency("com.snmodding.nautilus")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private const string PLUGIN_GUID = "00fae7dd-6469-4136-b0c6-34e0ebb067e5";
+        private string SERVER_URL;
+        public new static ManualLogSource Logger { get; private set; }
+        private static Assembly Assembly { get; } = Assembly.GetExecutingAssembly();
+
+        private GameData gameData = new GameData();
+
+        private HttpClient httpClient;
+        private void Awake()
+        {
+            // set project-scoped logger instance
+            Logger = base.Logger;
+
+            try
+            {
+                SERVER_URL = File.ReadLines(Environment.GetEnvironmentVariable("ProgramData") + "/Artemis/webserver.txt").First();
+            }
+            catch (Exception ex) {
+                Console.WriteLine(ex.ToString());
+            }
+
+            httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri(SERVER_URL);
+
+            // register harmony patches, if there are any
+            Harmony.CreateAndPatchAll(Assembly, $"{PluginInfo.PLUGIN_GUID}");
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+
+            Logger.LogInfo($"Artemis server URL is {SERVER_URL}");
+        }
+
+        
+        private async void Update()
+        {
+            Player player = Player.main;
+            WeatherManager weatherManager = WeatherManager.main;
+
+            if (player)
+            {
+                gameData.oxygen_available = player.GetOxygenAvailable();
+                gameData.oxygen_capacity = player.GetOxygenCapacity();
+                gameData.health = player.liveMixin.health;
+                gameData.max_health = player.liveMixin.maxHealth;
+                gameData.food = player.gameObject.GetComponent<Survival>().food;
+                gameData.water = player.gameObject.GetComponent<Survival>().water;
+                gameData.body_temperature = 100 - player.GetComponent<Survival>().bodyTemperature.playerColdValue.Value;
+                gameData.depth_level = player.depthLevel;
+                gameData.biome = player.CalculateBiome();
+                gameData.rain_intensity = weatherManager.rainIntensity.Value;
+                gameData.snow_intensity = weatherManager.snowIntensity.Value;
+                gameData.is_swimming = player.IsSwimming();
+                gameData.is_inside = player.IsInside();
+                gameData.PDA_state = player.GetPDA().state;
+
+
+            }
+            
+            SendGameDataToArtemisApi(gameData);
+            
+           }
+
+        private void OnApplicationQuit()
+        {
+            SendGameDataToArtemisApi(new GameData());
+        }
+        private void SendGameDataToArtemisApi(GameData gameData)
+        {
+            string json = JsonConvert.SerializeObject(gameData);
+            StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = httpClient.PostAsync("plugins/" + PLUGIN_GUID + "/SubnauticaBelowZero", content);
+        }
+
+    }
+}
